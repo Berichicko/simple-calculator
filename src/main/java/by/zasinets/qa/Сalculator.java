327945c17ce6eb0e8ded994bef74d36177afbe10
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-06-18 15:50:30 +0300

    simple solution

--- src/main/java/by/zasinets/qa/Сalculator.java
+++ src/main/java/by/zasinets/qa/Сalculator.java
@@ -6,17 +6,11 @@ public class Сalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите два числа ");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        if (scanner.hasNextLine()) {
-            int sum = (a + b);
-            int multiplication = (a * b);
-            System.out.println("Сумма двух чисел" + " " + sum);
-            System.out.println("Произведение двух чисел" + " " + multiplication);
-
-        }
-        scanner.close();
-
+        int numberFirst = scanner.nextInt();
+        int numberSecond = scanner.nextInt();
+        int resultSum = numberFirst * numberSecond;
+        System.out.println("Сумма двух чисел" + " " + resultSum);
+        int resultMultiply = numberFirst * numberSecond;
+        System.out.println("Произведение двух чисел" + " " + resultMultiply);
     }
-
 }
