0efd259dba70bc5dd4fc397ad134c5f9b52ce17d
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-06-20 17:54:16 +0300

    add Exception

--- src/main/java/by/zasinets/qa/Main.java
+++ src/main/java/by/zasinets/qa/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        try {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите два числа ");
         int numberFirst = scanner.nextInt();
@@ -14,8 +14,9 @@ public class Main {
         Multiplying multiplying = new Multiplying(numberFirst, numberSecond);
 
         System.out.println("Сумма двух чисел" + " " + sum.calculation());
-
         System.out.println("Произведение двух чисел" + " " + multiplying.calculation());
-
+        } catch (Exception e) {
+            System.out.println( "\u001B[36m"+ "Некорректно введено значение для математических операций! Попробуйте еще раз!");
+        }
     }
 }
