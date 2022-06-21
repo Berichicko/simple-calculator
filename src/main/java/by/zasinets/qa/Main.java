009df6b1627b507b13d17821a6a6128b4aaceb49
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-06-21 07:32:03 +0300

    add console output

--- src/main/java/by/zasinets/qa/Main.java
+++ src/main/java/by/zasinets/qa/Main.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите два числа ");
-        int numberFirst = scanner.nextInt();
-        int numberSecond = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите два числа ");
+            int numberFirst = scanner.nextInt();
+            int numberSecond = scanner.nextInt();
 
-        Sum sum = new Sum(numberFirst, numberSecond);
-        Multiplying multiplying = new Multiplying(numberFirst, numberSecond);
+            Sum sum = new Sum(numberFirst, numberSecond);
+            Multiplying multiplying = new Multiplying(numberFirst, numberSecond);
 
-        System.out.println("Сумма двух чисел" + " " + sum.calculation());
-        System.out.println("Произведение двух чисел" + " " + multiplying.calculation());
+            System.out.println("Сумма двух чисел: " + numberFirst + " + " + numberSecond + " = " + sum.calculation());
+            System.out.println("Произведение двух чисел: " + numberFirst + " * " + numberSecond + " = " + multiplying.calculation());
         } catch (Exception e) {
-            System.out.println( "\u001B[36m"+ "Некорректно введено значение для математических операций! Попробуйте еще раз!");
+            System.out.println("\u001B[36m" + "Некорректно введено значение для математических операций! Попробуйте еще раз!");
         }
     }
 }
